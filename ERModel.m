67da5d8209c7e233de7071e32d210fc9911edb89
ShanLu1984 <31110848+ShanLu1984@users.noreply.github.com>
2017-10-11 00:51:40 -0700

    Update ERModel.m

--- ERModel.m
+++ ERModel.m
@@ -1,19 +1,22 @@
-function A = ERModel(n,mean_deg)
+function A_er = ERModel(n,mean_deg)
 
 disp('Generating a Erdos-Renyi graph...')
 tic;
 
 p = mean_deg/n;
-A=zeros(n,n);
+A_er=zeros(n,n);
 
 for i=1:n
     for j=i+1:n
         rd=rand(1);
         if rd<=p
-            A(i,j)=1;
-            A(j,i)=1;
+            A_er(i,j)=1;
+            A_er(j,i)=1;
         end
     end
 end
+
+% Remove isolate nodes;
+A_er = remove0nodes(A_er);
 
 toc;
